534d8dac8747554f5feed699035bdf5800262344
Ritik Gupta <guptaritik349@gmail.com>
2020-08-01 02:16:45 +0530

    Restore db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,5 +1,4 @@
 <?php 
-  $con = mysqli_connect("ec2-54-80-184-43.compute-1.amazonaws.com
-", "vmzmuafedgealp", "32b75dd0612b19b40ee2338902d9b1654ac6d4ab753777af5e1e3ee46952e7c3");
-  mysqli_select_db($con, "de0os4adsmka6p");
+  $con = mysqli_connect("localhost", "root");
+  mysqli_select_db($con, "E_Medishop");
 ?>
